4d030b967d8afa8b58b427affe02565f32da7341
Avery Peterson <peterson.a@husky.neu.edu>
2019-01-22 12:12:28 -0500

    first commit

---
+++ src/main/java/edu/neu/cs4500/cs4500sp19s2acp9191/Cs4500Sp19s2Acp9191Application.java
@@ -0,0 +1,14 @@
+package edu.neu.cs4500.cs4500sp19s2acp9191;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs4500Sp19s2Acp9191Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Cs4500Sp19s2Acp9191Application.class, args);
+  }
+
+}
+
---
+++ src/main/java/edu/neu/cs4500/cs4500sp19s2acp9191/controllers/hello/HelloController.java
@@ -0,0 +1,18 @@
+package edu.neu.cs4500.cs4500sp19s2acp9191.controllers.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+  @RequestMapping("/api/hello/string")
+  public String sayHello() {
+    return "Hello CS4500!";
+  }
+
+  @RequestMapping("/api/hello/object")
+  public HelloObject sayHelloObject() {
+    HelloObject obj = new HelloObject("Hello Object CS4500!");
+    return obj;
+  }
+}
